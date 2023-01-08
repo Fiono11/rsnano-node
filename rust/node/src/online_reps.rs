897fd612d25f13a4492dcc783ee31694d906fa88
Fiono11 <ruipedromorais11@gmail.com>
2023-01-08 16:38:03 +0000

    WIP

--- rust/node/src/online_reps.rs
+++ rust/node/src/online_reps.rs
@@ -30,7 +30,6 @@ impl OnlineReps {
     pub fn new(ledger: Arc<Ledger>, node_config: Arc<NodeConfig>) -> Self {
 
         let transaction = ledger.store.tx_begin_read().unwrap();
-        //let trended_m = Arc::new(Mutex::new(Self::calculate_trend(transaction.txn(), &ledger, &node_config)));
 
         let mut online_reps = Self {
             ledger,
@@ -44,15 +43,11 @@ impl OnlineReps {
         let mut mutex = online_reps.trended_m.lock().unwrap();
         *mutex = online_reps.calculate_trend(transaction.txn());
         std::mem::drop(mutex);
-        println!("trended: {}", online_reps.trended_m.lock().unwrap().number());
         online_reps
     }
 
     pub fn calculate_online(&self) -> Amount {
-        //let mut current = Amount::zero();
         let mut current = 0;
-        //println!("REPS 1: {:?}", self.reps.by_account);
-        //let mutex = self.reps.lock().unwrap();
         for (i, e) in self.reps.entries.iter() {
             println!("REPS 3: {:?}", self.reps.by_account);
             println!("weight: {:?}", self.ledger.weight(&e.account));
@@ -66,29 +61,22 @@ impl OnlineReps {
     pub fn calculate_trend(&self, transaction_a: &dyn Transaction) -> Amount {
         let mut items = Vec::new();
         items.push(self.node_config.online_weight_minimum);
-        println!("items: {:?}", items);
         let mut it = self.ledger.store.online_weight().begin(transaction_a);
         while !it.is_end() {
             items.push(*it.current().unwrap().1);
             it.next();
         }
-        println!("items 2: {:?}", items);
         let median_idx = items.len() / 2;
         items.sort();
-        println!("items 3: {:?}", items);
         let result = items[median_idx];
-        println!("result 1 {}", result.number());
         return result;
     }
 
     pub fn observe(&mut self, rep_a: Account) {
-        println!("1111111");
         if self.ledger.weight(&rep_a).number() > 0 {
             let mut new_insert = true;
             //let mut mutex = self.reps.lock().unwrap();
-            println!("22222222");
             if let Some(id) = self.reps.by_account.get(&rep_a) {
-                println!("3333333");
                 let old_time = self.reps.entries.get(id).unwrap().time;
                 let mut ids = self.reps.by_time.get(&old_time).unwrap().clone();
                 let index = ids.iter().position(|x| x == id).unwrap();
@@ -98,7 +86,6 @@ impl OnlineReps {
                 self.reps.by_account.remove(&rep_a);
                 new_insert = false;
             }
-            println!("4444444");
             let start = SystemTime::now();
             let since_the_epoch = start
                 .duration_since(UNIX_EPOCH)
@@ -109,18 +96,9 @@ impl OnlineReps {
                 account: rep_a,
                 time,
             };
-            println!("555555");
             self.reps.insert(entry);
-            println!("time: {:?}", time);
-            //println!("period: {}", self.node_config.weight_period);
             let cutoff = since_the_epoch - Duration::from_secs(300);
-            println!("now: {:?}", since_the_epoch);
-            println!("period: {:?}", Duration::from_secs(300));
-            println!("cutoff: {:?}", cutoff);
-            //let oldest = self.reps.by_time.first_key_value().unwrap().0;
-            //let trimmed = oldest < cutoff;
             let trimmed = self.reps.trim(Duration::from_secs(300));
-            //println!("REPS 2: {:?}", self.reps.by_account);
             if new_insert || trimmed {
                 let mut mutex = self.online_m.lock().unwrap();
                 *mutex = self.calculate_online();
@@ -140,7 +118,6 @@ impl OnlineReps {
             .duration_since(UNIX_EPOCH)
             .expect("Time went backwards");
         self.ledger.store.online_weight().put(transaction.as_mut(), since_the_epoch.as_secs(), &self.online_m.lock().unwrap());
-        println!("minimum: {}", self.node_config.online_weight_minimum.number());
         let mut mutex = self.trended_m.lock().unwrap();
         *mutex = self.calculate_trend(transaction.txn());
     }
@@ -151,7 +128,6 @@ impl OnlineReps {
 
     pub fn online(&self) -> Amount {
         let online = *self.online_m.lock().unwrap();
-        println!("online: {}", online.clone().number());
         online
     }
 
@@ -167,9 +143,6 @@ impl OnlineReps {
     }
 
     pub fn clear(&mut self) {
-        //let mut mutex1 = self.reps.lock().unwrap();
-        //mutex1.clear();
-        //std::mem::drop(mutex1);
         self.reps = EntryContainer::new();
         let mut mutex2 = self.online_m.lock().unwrap();
         *mutex2 = Amount::zero();
@@ -190,7 +163,6 @@ struct EntryContainer {
     by_account: HashMap<Account, usize>,
     by_time: BTreeMap<Instant, Vec<usize>>,
     next_id: usize,
-    //empty_id_set: HashSet<usize>,
 }
 
 impl EntryContainer {
@@ -209,8 +181,6 @@ impl EntryContainer {
         let id = self.create_id();
 
         self.by_account.insert(entry.account, id);
-        //let by_account = self.by_account.entry(entry.account).or_default();
-        //by_account.insert(id);
 
         let by_time = self.by_time.entry(entry.time).or_default();
         by_time.push(id);
@@ -230,10 +200,6 @@ impl EntryContainer {
             Some(id) => Some(*id),
             None => None,
         }
-    }
-
-    fn iter_entries<'a>(&'a self, ids: &'a HashSet<usize>) -> impl Iterator<Item = &Entry> + 'a {
-        ids.iter().map(|&id| &self.entries[&id])
     }
 
     fn trim(&mut self, upper_bound: Duration) -> bool {
@@ -265,16 +231,6 @@ impl EntryContainer {
         trimmed
     }
 
-    /*fn change_time_for_account(&mut self, account: &Account, time: Instant) -> bool {
-        match self.by_account.get(account) {
-            Some(ids) => {
-                change_time_for_entries(ids, time, &mut self.entries, &mut self.by_time);
-                true
-            }
-            None => false,
-        }
-    }*/
-
     fn len(&self) -> usize {
         self.entries.len()
     }
@@ -285,42 +241,4 @@ struct Entry {
     time: Instant,
 }
 
-/*fn change_time_for_entries(
-    ids: &HashSet<usize>,
-    time: Instant,
-    entries: &mut HashMap<usize, Entry>,
-    by_time: &mut BTreeMap<Instant, Vec<usize>>,
-) {
-    for id in ids {
-        change_time_for_entry(id, time, entries, by_time);
-    }
-}*/
-
-/*fn change_time_for_entry(
-    id: &usize,
-    time: Instant,
-    entries: &mut HashMap<usize, Entry>,
-    by_time: &mut BTreeMap<Instant, Vec<usize>>,
-) {
-    if let Some(entry) = entries.get_mut(id) {
-        let old_time = entry.time;
-        entry.time = time;
-        remove_from_time_index(old_time, id, by_time);
-        by_time.entry(time).or_default().push(*id);
-    }
-}*/
-
-fn remove_from_time_index(
-    time: Instant,
-    id: &usize,
-    ids_by_time: &mut BTreeMap<Instant, Vec<usize>>,
-) {
-    if let Some(ids) = ids_by_time.get_mut(&time) {
-        if ids.len() == 1 {
-            ids_by_time.remove(&time);
-        } else {
-            ids.retain(|x| x != id);
-        }
-    }
-}
 
