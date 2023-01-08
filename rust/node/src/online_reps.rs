4aee6215223f068cbafe3c80991c23037de727c5
Fiono11 <ruipedromorais11@gmail.com>
2023-01-08 19:19:23 +0000

    WIP

--- rust/node/src/online_reps.rs
+++ rust/node/src/online_reps.rs
@@ -1,5 +1,6 @@
 use std::cmp::max;
 use std::collections::{BTreeMap, HashMap, HashSet};
+use std::mem;
 use std::mem::size_of;
 use std::ops::Deref;
 use std::process::id;
@@ -14,6 +15,7 @@ use crate::config::NodeConfig;
 use crate::stats::DetailType::Send;
 use crate::voting::VoteSpacing;
 use primitive_types::U256;
+use rsnano_store_lmdb::get;
 
 pub const ONLINE_WEIGHT_QUORUM: u8 = 67;
 
@@ -48,12 +50,8 @@ impl OnlineReps {
 
     pub fn calculate_online(&self) -> Amount {
         let mut current = 0;
-        for (i, e) in self.reps.entries.iter() {
-            println!("REPS 3: {:?}", self.reps.by_account);
-            println!("weight: {:?}", self.ledger.weight(&e.account));
-            //if let Some(e) = &self.reps.entries.get(&i) {
-                current += self.ledger.weight(&e.account).number();
-            //}
+        for (_, e) in self.reps.entries.lock().unwrap().iter() {
+            current += self.ledger.weight(&e.account).number();
         }
         Amount::new(current)
     }
@@ -75,17 +73,22 @@ impl OnlineReps {
     pub fn observe(&mut self, rep_a: Account) {
         if self.ledger.weight(&rep_a).number() > 0 {
             let mut new_insert = true;
-            //let mut mutex = self.reps.lock().unwrap();
-            if let Some(id) = self.reps.by_account.get(&rep_a) {
-                let old_time = self.reps.entries.get(id).unwrap().time;
-                let mut ids = self.reps.by_time.get(&old_time).unwrap().clone();
+            let mut mutex1 = self.reps.by_account.lock().unwrap();
+            let mut mutex2 = self.reps.by_time.lock().unwrap();
+            let mut mutex3 = self.reps.entries.lock().unwrap();
+            if let Some(id) = mutex1.get(&rep_a) {
+                let old_time = mutex3.get(id).unwrap().time;
+                let mut ids = mutex2.get(&old_time).unwrap().clone();
                 let index = ids.iter().position(|x| x == id).unwrap();
                 ids.remove(index);
-                self.reps.by_time.insert(old_time, ids.to_owned());
-                self.reps.entries.remove(id).unwrap();
-                self.reps.by_account.remove(&rep_a);
+                mutex2.insert(old_time, ids.to_owned());
+                mutex3.remove(id).unwrap();
                 new_insert = false;
             }
+            mutex1.remove(&rep_a);
+            mem::drop(mutex1);
+            mem::drop(mutex2);
+            mem::drop(mutex3);
             let start = SystemTime::now();
             let since_the_epoch = start
                 .duration_since(UNIX_EPOCH)
@@ -139,7 +142,7 @@ impl OnlineReps {
     }
 
     pub fn list(&self) -> Vec<Account> {
-        self.reps.by_account.iter().map(|(a, b)| *a).collect()
+        self.reps.by_account.lock().unwrap().iter().map(|(a, b)| *a).collect()
     }
 
     pub fn clear(&mut self) {
@@ -159,10 +162,10 @@ impl OnlineReps {
 
 #[derive(Default)]
 struct EntryContainer {
-    entries: HashMap<usize, Entry>,
-    by_account: HashMap<Account, usize>,
-    by_time: BTreeMap<Instant, Vec<usize>>,
-    next_id: usize,
+    entries: Arc<Mutex<HashMap<usize, Entry>>>,
+    by_account: Arc<Mutex<HashMap<Account, usize>>>,
+    by_time: Arc<Mutex<BTreeMap<Instant, Vec<usize>>>>,
+    next_id: Arc<Mutex<usize>>,
 }
 
 impl EntryContainer {
@@ -171,32 +174,32 @@ impl EntryContainer {
     }
 
     pub fn clear(&mut self) {
-        self.entries = HashMap::new();
-        self.by_account = HashMap::new();
-        self.by_time = BTreeMap::new();
-        self.next_id = 0;
+        self.entries = Arc::new(Mutex::new(HashMap::new()));
+        self.by_account = Arc::new(Mutex::new(HashMap::new()));
+        self.by_time = Arc::new(Mutex::new(BTreeMap::new()));
+        self.next_id = Arc::new(Mutex::new(0));
     }
 
     pub fn insert(&mut self, entry: Entry) {
         let id = self.create_id();
 
-        self.by_account.insert(entry.account, id);
+        self.by_account.lock().unwrap().insert(entry.account, id);
 
-        let by_time = self.by_time.entry(entry.time).or_default();
+        let mut binding = self.by_time.lock().unwrap();
+        let mut by_time = binding.entry(entry.time).or_default() ;
         by_time.push(id);
 
-        self.entries.insert(id, entry);
-        println!("REPS: {:?}", self.by_account);
+        self.entries.lock().unwrap().insert(id, entry);
     }
 
     fn create_id(&mut self) -> usize {
-        let id = self.next_id;
-        self.next_id = self.next_id.wrapping_add(1);
-        id
+        let mut id = self.next_id.lock().unwrap();
+        *id += 1;
+        *id
     }
 
     pub fn by_account(&self, account: &Account) -> Option<usize> {
-        match self.by_account.get(account) {
+        match self.by_account.lock().unwrap().get(account) {
             Some(id) => Some(*id),
             None => None,
         }
@@ -205,7 +208,7 @@ impl EntryContainer {
     fn trim(&mut self, upper_bound: Duration) -> bool {
         let mut trimmed = false;
         let mut instants_to_remove = Vec::new();
-        for (&instant, ids) in self.by_time.iter() {
+        for (&instant, ids) in self.by_time.lock().unwrap().iter() {
             if instant.elapsed() < upper_bound {
                 break;
             }
@@ -213,26 +216,21 @@ impl EntryContainer {
             instants_to_remove.push(instant);
 
             for id in ids {
-                let entry = self.entries.remove(id).unwrap();
-                self.by_account.remove(&entry.account).unwrap();
-                //let by_account = self.by_account.get_mut(&entry.account).unwrap();
-                //by_account.remove(id);
-                //if by_account.is_empty() {
-                    //self.by_account.remove(&entry.account);
-                //}
+                let entry = self.entries.lock().unwrap().remove(id).unwrap();
+                self.by_account.lock().unwrap().remove(&entry.account).unwrap();
             }
             trimmed = true;
         }
 
         for instant in instants_to_remove {
-            self.by_time.remove(&instant);
+            self.by_time.lock().unwrap().remove(&instant);
         }
 
         trimmed
     }
 
     fn len(&self) -> usize {
-        self.entries.len()
+        self.entries.lock().unwrap().len()
     }
 }
 
