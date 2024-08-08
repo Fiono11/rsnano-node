d6c211a73bd11fd672b4d44134d13cee864c25a2
Fiono <ruipedromorais11@gmail.com>
2024-08-08 13:19:43 +0100

    Set header

--- rust/rpc/src/lib.rs
+++ rust/rpc/src/lib.rs
@@ -1,56 +1,60 @@
+use axum::{extract::State, response::IntoResponse, routing::post, Json};
+use axum::{
+    http::{Request, StatusCode},
+    middleware::map_request,
+    Router,
+};
 use rsnano_node::node::Node;
 use serde::{Deserialize, Serialize};
+use std::net::SocketAddr;
 use std::net::{IpAddr, Ipv4Addr};
 use std::sync::Arc;
-use tokio::signal;
-use warp::Filter;
-
-pub async fn run_server(node: Arc<Node>) -> anyhow::Result<()> {
-    let service = Service(node);
-
-    let rpc_route = warp::path::end()
-        .and(warp::post())
-        .and(warp::body::json())
-        .and_then(move |rpc_request: RpcRequest| {
-            let service = service.clone();
-            async move {
-                if rpc_request.action == "version" {
-                    let response = service.version().await;
-                    let json_response = warp::reply::json(&RpcResponse { message: response });
-                    Ok::<_, warp::Rejection>(json_response)
-                } else {
-                    let error_response = warp::reply::json(&"Invalid action".to_string());
-                    Ok::<_, warp::Rejection>(error_response)
-                }
-            }
-        });
-
-    let server_addr = (IpAddr::V4(Ipv4Addr::LOCALHOST), 7076);
-    let (addr, server) = warp::serve(rpc_route).bind_with_graceful_shutdown(server_addr, async {
-        signal::ctrl_c()
-            .await
-            .expect("Failed to listen for ctrl+c signal");
-    });
-
-    println!("Server running on http://{}", addr);
-    server.await;
-
-    Ok(())
-}
-
-//#[tarpc::service]
-pub trait RpcService {
-    async fn version(&self) -> String;
-}
 
 #[derive(Clone)]
 struct Service(Arc<Node>);
 
-impl RpcService for Service {
-    async fn version(&self) -> String {
-        let mut txn = self.0.store.env.tx_begin_read();
-        let version = self.0.store.version.get(&mut txn);
-        format!("store_version: {}", version.unwrap()).to_string()
+async fn set_header<B>(mut request: Request<B>) -> Request<B> {
+    request
+        .headers_mut()
+        .insert("Content-Type", "application/json".parse().unwrap());
+    request
+}
+
+pub async fn run_server(node: Arc<Node>) -> anyhow::Result<()> {
+    let service = Service(node);
+
+    let app = Router::new()
+        .route("/", post(handle_rpc))
+        .layer(map_request(set_header))
+        .with_state(service);
+
+    let server_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 7076);
+
+    println!("Server running on http://{}", server_addr);
+
+    let listener = tokio::net::TcpListener::bind(server_addr).await.unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+
+    Ok(())
+}
+
+async fn handle_rpc(
+    State(service): State<Service>,
+    Json(rpc_request): Json<RpcRequest>,
+) -> impl IntoResponse {
+    match rpc_request.action.as_str() {
+        "version" => {
+            let response = service.version().await;
+            let json_response = Json(RpcResponse { message: response });
+            (StatusCode::OK, json_response).into_response()
+        }
+        _ => {
+            let error_response = Json(RpcResponse {
+                message: "Invalid action".to_string(),
+            });
+            (StatusCode::BAD_REQUEST, error_response).into_response()
+        }
     }
 }
 
@@ -62,4 +66,18 @@ struct RpcRequest {
 #[derive(Serialize)]
 struct RpcResponse {
     message: String,
+}
+
+#[async_trait::async_trait]
+pub trait RpcService {
+    async fn version(&self) -> String;
+}
+
+#[async_trait::async_trait]
+impl RpcService for Service {
+    async fn version(&self) -> String {
+        let mut txn = self.0.store.env.tx_begin_read();
+        let version = self.0.store.version.get(&mut txn);
+        format!("store_version: {}", version.unwrap()).to_string()
+    }
 }
