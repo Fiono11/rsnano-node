74c8f7ae64232abb3e3463003ecc132331fd326b
Gustav Schauwecker <service@simpago.com>
2025-11-21 13:36:49 +0100

    Remove tokio guard again

--- nullables/lmdb/src/environment.rs
+++ nullables/lmdb/src/environment.rs
@@ -130,10 +130,6 @@ impl LmdbEnvironment {
     }
 
     pub fn begin_read(&self) -> ReadTransaction {
-        #[cfg(feature = "tokio_guard")]
-        {
-            ensure_not_in_tokio_thread();
-        }
         match &self.env_strategy {
             EnvironmentStrategy::Real(s) => s.begin_read(),
             EnvironmentStrategy::Nulled(s) => s.begin_read(),
@@ -141,10 +137,6 @@ impl LmdbEnvironment {
     }
 
     pub fn begin_write(&self) -> WriteTransaction {
-        #[cfg(feature = "tokio_guard")]
-        {
-            ensure_not_in_tokio_thread();
-        }
         match &self.env_strategy {
             EnvironmentStrategy::Real(s) => s.begin_write(),
             EnvironmentStrategy::Nulled(s) => s.begin_write(),
@@ -361,15 +353,6 @@ impl EnvironmentStubBuilder {
 
     pub fn build(self) -> LmdbEnvironment {
         LmdbEnvironment::new_null_with_data(self.databases)
-    }
-}
-
-#[cfg(feature = "tokio_guard")]
-fn ensure_not_in_tokio_thread() {
-    if let Some(name) = std::thread::current().name() {
-        if name.contains("tokio") {
-            panic!("LMDB transactions must not be started from a Tokio thread!");
-        }
     }
 }
 
