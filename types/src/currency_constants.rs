b3941a52436d42a6d8ac838f64103cca220de800
Gustav Schauwecker <service@simpago.com>
2025-11-28 16:46:29 +0100

    Extract currency_constants

--- types/src/currency_constants.rs
+++ types/src/currency_constants.rs
@@ -27,3 +27,17 @@ pub const WORKING_PATH_PREFIX: &str = "Nano";
 pub const PEERING_LIVE: &str = "peering.nano.org";
 pub const PEERING_BETA: &str = "peering-beta.nano.org";
 pub const PEERING_TEST: &str = "peering-test.nano.org";
+
+pub const PRECONFIGURED_REPRESENTATIVES_LIVE: [&'static str; 8] = [
+    "nano_3arg3asgtigae3xckabaaewkx3bzsh7nwz7jkmjos79ihyaxwphhm6qgjps4",
+    "nano_1stofnrxuz3cai7ze75o174bpm7scwj9jn3nxsn8ntzg784jf1gzn1jjdkou",
+    "nano_1q3hqecaw15cjt7thbtxu3pbzr1eihtzzpzxguoc37bj1wc5ffoh7w74gi6p",
+    "nano_3dmtrrws3pocycmbqwawk6xs7446qxa36fcncush4s1pejk16ksbmakis78m",
+    "nano_3hd4ezdgsp15iemx7h81in7xz5tpxi43b6b41zn3qmwiuypankocw3awes5k",
+    "nano_1awsn43we17c1oshdru4azeqjz9wii41dy8npubm4rg11so7dx3jtqgoeahy",
+    "nano_1anrzcuwe64rwxzcco8dkhpyxpi8kd7zsjc1oeimpc3ppca4mrjtwnqposrs",
+    "nano_1hza3f7wiiqa7ig3jczyxj5yo86yegcmqk3criaz838j91sxcckpfhbhhra1",
+];
+
+pub const PRECONFIGURED_REPRESENTATIVES_BETA: [&'static str; 1] =
+    ["nano_1defau1t9off1ine9rep99999999999999999999999999999999wgmuzxxy"];
--- wallet/src/config.rs
+++ wallet/src/config.rs
@@ -1,5 +1,8 @@
 use rsnano_ledger::DEV_GENESIS_PUB_KEY;
-use rsnano_types::{Account, Amount, Networks, PublicKey};
+use rsnano_types::{
+    Account, Amount, Networks, PublicKey, PRECONFIGURED_REPRESENTATIVES_BETA,
+    PRECONFIGURED_REPRESENTATIVES_LIVE,
+};
 use std::time::Duration;
 
 #[derive(Clone)]
@@ -49,11 +52,10 @@ impl WalletsConfig {
 
     pub fn defaults_beta() -> Self {
         Self {
-            preconfigured_representatives: vec![
-                Account::parse("nano_1defau1t9off1ine9rep99999999999999999999999999999999wgmuzxxy")
-                    .unwrap()
-                    .into(),
-            ],
+            preconfigured_representatives: PRECONFIGURED_REPRESENTATIVES_BETA
+                .iter()
+                .map(|r| Account::parse(r).unwrap().into())
+                .collect(),
             ..Self::defaults_live()
         }
     }
@@ -73,19 +75,20 @@ impl Default for WalletsConfig {
 }
 
 pub fn default_preconfigured_representatives_for_live() -> Vec<PublicKey> {
-    const REP_KEYS: [&'static str; 8] = [
-        "A30E0A32ED41C8607AA9212843392E853FCBCB4E7CB194E35C94F07F91DE59EF",
-        "67556D31DDFC2A440BF6147501449B4CB9572278D034EE686A6BEE29851681DF",
-        "5C2FBB148E006A8E8BA7A75DD86C9FE00C83F5FFDBFD76EAA09531071436B6AF",
-        "AE7AC63990DAAAF2A69BF11C913B928844BF5012355456F2F164166464024B29",
-        "BD6267D6ECD8038327D2BCC0850BDF8F56EC0414912207E81BCF90DFAC8A4AAA",
-        "2399A083C600AA0572F5E36247D978FCFC840405F8D4B6D33161C0066A55F431",
-        "2298FAB7C61058E77EA554CB93EDEEDA0692CBFCC540AB213B2836B29029E23A",
-        "3FE80B4BC842E82C1C18ABFEEC47EA989E63953BC82AC411F304D13833D52A56",
-    ];
-
-    REP_KEYS
+    PRECONFIGURED_REPRESENTATIVES_LIVE
         .iter()
-        .map(|s| PublicKey::decode_hex(s).unwrap())
+        .map(|s| Account::parse(s).unwrap().into())
         .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn print_preconfigured_reps() {
+        for key in default_preconfigured_representatives_for_live() {
+            println!("{}", key.as_account().encode_account());
+        }
+    }
 }
