1e3fb52cbbe12dce0cd688d9a76bdd718561308c
Gustav Schauwecker <service@simpago.com>
2026-02-26 16:58:32 +0100

    Track spawns of rate limited threads

--- node/src/block_processing/bounded_backlog/rate_limit_thread.rs
+++ node/src/block_processing/bounded_backlog/rate_limit_thread.rs
@@ -1,7 +1,11 @@
-use std::time::Duration;
+use std::{
+    sync::{Arc, Mutex},
+    time::Duration,
+};
 
 use rsnano_network::token_bucket::TokenBucket;
 use rsnano_nullable_clock::SteadyClock;
+use rsnano_output_tracker::{OutputListenerMt, OutputTrackerMt};
 use rsnano_utils::{
     CancellationToken,
     thread_factory::{JoinHandle, ThreadFactory},
@@ -12,6 +16,7 @@ use rsnano_utils::{
 pub(crate) struct RateLimitThreadFactory {
     thread_factory: ThreadFactory,
     scan_loop: RateLimitLoop,
+    spawn_listener: OutputListenerMt<SpawnEvent>,
 }
 
 impl RateLimitThreadFactory {
@@ -26,10 +31,25 @@ impl RateLimitThreadFactory {
     where
         F: FnMut() + Send + 'static,
     {
+        let callback: Option<Box<dyn FnMut() + Send>> = Some(Box::new(f));
+        let callback = Arc::new(Mutex::new(callback));
+
+        let name = name.into();
+        self.spawn_listener.emit(SpawnEvent {
+            thread_name: name.clone(),
+            callback: callback.clone(),
+        });
         let scan_loop = self.scan_loop.clone();
         self.thread_factory.spawn(name, move || {
-            scan_loop.run(rate, batch_size, &cancel_token, f);
+            if let Some(callback_func) = callback.lock().unwrap().take() {
+                scan_loop.run(rate, batch_size, &cancel_token, callback_func);
+            }
         })
+    }
+
+    #[allow(dead_code)]
+    pub fn track_spawns(&self) -> Arc<OutputTrackerMt<SpawnEvent>> {
+        self.spawn_listener.track()
     }
 }
 
@@ -55,7 +75,7 @@ impl RateLimitLoop {
 
     fn run<F>(mut self, rate: usize, batch_size: usize, cancel_token: &CancellationToken, mut f: F)
     where
-        F: FnMut(),
+        F: FnMut() + Send + 'static,
     {
         self.limiter.reset_with(rate, rate);
 
@@ -79,6 +99,23 @@ impl RateLimitLoop {
     }
 }
 
+#[derive(Clone)]
+#[allow(dead_code)]
+pub(crate) struct SpawnEvent {
+    pub(crate) thread_name: String,
+    pub(crate) callback: Arc<Mutex<Option<Box<dyn FnMut() + Send>>>>,
+}
+
+impl SpawnEvent {
+    #[allow(dead_code)]
+    pub fn run(&self) {
+        let Some(mut callback) = self.callback.lock().unwrap().take() else {
+            panic!("No callback to call");
+        };
+        callback();
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -92,6 +129,7 @@ mod tests {
         let factory = RateLimitThreadFactory {
             thread_factory: ThreadFactory::new_null(),
             scan_loop: RateLimitLoop::new_null(),
+            spawn_listener: Default::default(),
         };
 
         let spawn_tracker = factory.thread_factory.track_spawns();
@@ -115,10 +153,11 @@ mod tests {
     }
 
     #[test]
-    fn loops_until_limitier_kicks_in() {
+    fn loops_until_limiter_kicks_in() {
         let factory = RateLimitThreadFactory {
             thread_factory: ThreadFactory::new_null(),
             scan_loop: RateLimitLoop::new_null(),
+            spawn_listener: Default::default(),
         };
 
         let spawn_tracker = factory.thread_factory.track_spawns();
@@ -139,5 +178,32 @@ mod tests {
 
         spawn.run();
         assert_eq!(call_counter.load(Ordering::Relaxed), 3);
+    }
+
+    #[test]
+    fn spawns_can_be_tracked() {
+        let factory = RateLimitThreadFactory {
+            thread_factory: ThreadFactory::new_null(),
+            scan_loop: RateLimitLoop::new_null(),
+            spawn_listener: Default::default(),
+        };
+
+        let spawn_tracker = factory.track_spawns();
+        let cancel_token = CancellationToken::new_null();
+        let call_counter = Arc::new(AtomicUsize::new(0));
+        let call_counter2 = call_counter.clone();
+        let _ = factory.spawn("testthread", cancel_token, 1, 1, move || {
+            call_counter2.fetch_add(1, Ordering::Relaxed);
+        });
+
+        let spawn = spawn_tracker
+            .output()
+            .pop()
+            .expect("should have spawned thread");
+
+        assert_eq!(spawn.thread_name, "testthread");
+        assert_eq!(call_counter.load(Ordering::Relaxed), 0);
+        spawn.run();
+        assert_eq!(call_counter.load(Ordering::Relaxed), 1);
     }
 }
