b8859abd8dada357cb9e9f3a576a6c60f9866b57
Gustav Schauwecker <service@simpago.com>
2026-03-29 13:40:23 +0200

    Tidy: Remove unused code

--- node/src/consensus/active_elections/aec_service.rs
+++ node/src/consensus/active_elections/aec_service.rs
@@ -5,9 +5,7 @@ use std::{
 };
 
 use rsnano_nullable_clock::Timestamp;
-use rsnano_types::{
-    Amount, Block, BlockHash, PublicKey, QualifiedRoot, SavedBlock, TimePriority, VoteError,
-};
+use rsnano_types::{Amount, Block, BlockHash, PublicKey, QualifiedRoot, SavedBlock, VoteError};
 use rsnano_utils::{
     container_info::{ContainerInfo, ContainerInfoProvider},
     stats::{StatsCollection, StatsSource},
@@ -18,7 +16,7 @@ use super::{
     ActiveElectionsConfig, ActiveElectionsContainer, ActiveElectionsInfo, AecCooldownReason,
     AecFact, AecInsertError, AecInsertRequest, ApplyVoteArgs,
 };
-use crate::consensus::election::{ConfirmedElection, Election, ElectionBehavior, VoteType};
+use crate::consensus::election::{ConfirmedElection, Election, ElectionBehavior};
 
 pub struct AecService {
     aec: RwLock<ActiveElectionsContainer>,
@@ -87,18 +85,6 @@ impl AecService {
         self.aec.read().unwrap().count_by_behavior(behavior)
     }
 
-    pub fn bucket_len(&self, bucket_id: usize) -> usize {
-        self.aec.read().unwrap().bucket_len(bucket_id)
-    }
-
-    pub fn find_bucket(&self, root: &QualifiedRoot) -> Option<usize> {
-        self.aec.read().unwrap().find_bucket(root)
-    }
-
-    pub fn lowest_priority(&self, bucket_id: usize) -> Option<(QualifiedRoot, TimePriority)> {
-        self.aec.read().unwrap().lowest_priority(bucket_id)
-    }
-
     pub fn vacancy(&self) -> i64 {
         self.aec.read().unwrap().vacancy()
     }
@@ -129,13 +115,6 @@ impl AecService {
         self.aec.write().unwrap().try_add_fork(fork, fork_tally)
     }
 
-    pub fn set_last_voted(&self, root: &QualifiedRoot, vote_type: VoteType, timestamp: Timestamp) {
-        self.aec
-            .write()
-            .unwrap()
-            .set_last_voted(root, vote_type, timestamp)
-    }
-
     pub fn apply_vote<'a>(
         &self,
         args: ApplyVoteArgs<'a>,
@@ -159,19 +138,8 @@ impl AecService {
         self.aec.write().unwrap().remove_votes(root, voters)
     }
 
-    pub fn erase_ended_elections(&self) {
-        self.aec.write().unwrap().erase_ended_elections()
-    }
-
     pub fn erase(&self, root: &QualifiedRoot) -> bool {
         self.aec.write().unwrap().erase(root)
-    }
-
-    pub fn erase_lowest_prio_election(&self, bucket_id: usize) {
-        self.aec
-            .write()
-            .unwrap()
-            .erase_lowest_prio_election(bucket_id)
     }
 
     pub fn confirm_dependent_elections(
