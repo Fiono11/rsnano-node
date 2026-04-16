8ae316ca8f5ed09cf7189e687da2195e8f6d144f
Gustav Schauwecker <service@simpago.com>
2026-04-16 10:23:05 +0200

    bootstrap: Tidy

---
+++ node/src/bootstrap/bootstrapper/state/bootstrap_queue/bootstrapping_account.rs
@@ -0,0 +1,94 @@
+use std::collections::VecDeque;
+
+use rsnano_nullable_clock::Timestamp;
+use rsnano_types::{Account, Block, BlockHash};
+
+use super::Priority;
+
+/// An account that is currently being bootstrapped
+#[derive(Clone, Default, PartialEq, Eq, Debug)]
+pub(crate) struct BootstrappingAccount {
+    pub account: Account,
+    pub priority: Priority,
+    pub fails: usize,
+    pub last_request: Option<Timestamp>,
+    pub blocked: Option<BlockedInfo>,
+    pub blocks: VecDeque<Block>,
+    pub state: AccountState,
+}
+
+impl BootstrappingAccount {
+    pub fn new(account: Account, priority: Priority) -> Self {
+        if account.is_zero() {
+            panic!("The zero account can never be a boostrapping account!")
+        }
+        Self {
+            account,
+            priority,
+            fails: 0,
+            last_request: None,
+            blocked: None,
+            blocks: VecDeque::new(),
+            state: AccountState::EnqueuedForDownload,
+        }
+    }
+
+    #[cfg(test)]
+    pub fn new_test_instance() -> Self {
+        Self {
+            account: Account::from(7),
+            priority: Priority::new(3.0),
+            fails: 0,
+            last_request: None,
+            blocked: None,
+            blocks: VecDeque::new(),
+            state: AccountState::EnqueuedForDownload,
+        }
+    }
+
+    #[cfg(test)]
+    pub fn new_blocked_test_instance() -> Self {
+        Self {
+            account: Account::from(7),
+            priority: Priority::new(3.0),
+            fails: 0,
+            last_request: None,
+            blocked: Some(BlockedInfo::new_test_instance()),
+            blocks: VecDeque::new(),
+            state: AccountState::Blocked,
+        }
+    }
+
+    pub fn first_block_hash(&self) -> Option<BlockHash> {
+        let front = self.blocks.front()?;
+        Some(front.hash())
+    }
+}
+
+#[derive(PartialEq, Eq, Clone, Copy, Debug, Default)]
+pub(crate) enum AccountState {
+    #[default]
+    EnqueuedForDownload,
+    Downloading,
+    ReadyToProcess,
+    Processing,
+    Blocked,
+}
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct BlockedInfo {
+    pub dependency_block: BlockHash,
+    /// Account that contains the dependency block, fetched via a background dependency walker
+    pub dependency_account: Option<Account>,
+    pub blocked_at: Timestamp,
+}
+
+impl BlockedInfo {
+    pub fn new_test_instance() -> Self {
+        Self {
+            dependency_block: BlockHash::from(456),
+            dependency_account: None,
+            blocked_at: Timestamp::new_test_instance(),
+        }
+    }
+}
--- node/src/bootstrap/bootstrapper/state/bootstrap_queue/mod.rs
+++ node/src/bootstrap/bootstrapper/state/bootstrap_queue/mod.rs
@@ -1,110 +1,25 @@
 mod blocked;
+mod bootstrapping_account;
 mod download_queue;
 mod downloading;
 mod priority;
 mod single_block_account_set;
 
+pub use priority::Priority;
+
 use std::{cmp::min, collections::VecDeque, time::Duration};
+
+use rustc_hash::FxHashMap;
 
 use rsnano_nullable_clock::Timestamp;
 use rsnano_types::{Account, Block, BlockHash};
 use rsnano_utils::container_info::ContainerInfo;
 
-use crate::bootstrap::bootstrapper::state::bootstrap_queue::{
-    downloading::DownloadingAccounts, single_block_account_set::SingleBlockAccountSet,
-};
 use blocked::BlockedAccounts;
+use bootstrapping_account::{AccountState, BlockedInfo, BootstrappingAccount};
 use download_queue::{ChangePriorityResult, DownloadQueue};
-pub use priority::Priority;
-use rustc_hash::FxHashMap;
-
-/// An account that is currently being bootstrapped
-#[derive(Clone, Default, PartialEq, Eq, Debug)]
-pub(crate) struct BootstrappingAccount {
-    pub account: Account,
-    pub priority: Priority,
-    pub fails: usize,
-    pub last_request: Option<Timestamp>,
-    pub blocked: Option<BlockedInfo>,
-    pub blocks: VecDeque<Block>,
-    pub state: AccountState,
-}
-
-impl BootstrappingAccount {
-    pub fn new(account: Account, priority: Priority) -> Self {
-        if account.is_zero() {
-            panic!("The zero account can never be a boostrapping account!")
-        }
-        Self {
-            account,
-            priority,
-            fails: 0,
-            last_request: None,
-            blocked: None,
-            blocks: VecDeque::new(),
-            state: AccountState::EnqueuedForDownload,
-        }
-    }
-
-    #[cfg(test)]
-    pub fn new_test_instance() -> Self {
-        Self {
-            account: Account::from(7),
-            priority: Priority::new(3.0),
-            fails: 0,
-            last_request: None,
-            blocked: None,
-            blocks: VecDeque::new(),
-            state: AccountState::EnqueuedForDownload,
-        }
-    }
-
-    #[cfg(test)]
-    pub fn new_blocked_test_instance() -> Self {
-        Self {
-            account: Account::from(7),
-            priority: Priority::new(3.0),
-            fails: 0,
-            last_request: None,
-            blocked: Some(BlockedInfo::new_test_instance()),
-            blocks: VecDeque::new(),
-            state: AccountState::Blocked,
-        }
-    }
-
-    pub fn first_block_hash(&self) -> Option<BlockHash> {
-        let front = self.blocks.front()?;
-        Some(front.hash())
-    }
-}
-
-#[derive(PartialEq, Eq, Clone, Copy, Debug, Default)]
-pub(crate) enum AccountState {
-    #[default]
-    EnqueuedForDownload,
-    Downloading,
-    ReadyToProcess,
-    Processing,
-    Blocked,
-}
-
-#[derive(Clone, PartialEq, Eq, Debug)]
-pub struct BlockedInfo {
-    pub dependency_block: BlockHash,
-    /// Account that contains the dependency block, fetched via a background dependency walker
-    pub dependency_account: Option<Account>,
-    pub blocked_at: Timestamp,
-}
-
-impl BlockedInfo {
-    pub fn new_test_instance() -> Self {
-        Self {
-            dependency_block: BlockHash::from(456),
-            dependency_account: None,
-            blocked_at: Timestamp::new_test_instance(),
-        }
-    }
-}
+use downloading::DownloadingAccounts;
+use single_block_account_set::SingleBlockAccountSet;
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct BootstrapQueueConfig {
